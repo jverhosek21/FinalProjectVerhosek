3f2bdaf5e6179bbe709b5ba4c9f420038e871985
jverhosek21 <74193825+jverhosek21@users.noreply.github.com>
2021-03-28 16:13:37 -0400

    pawn piece moves properly and pictures updated

---
+++ Bishop.java
@@ -0,0 +1,18 @@
+import java.awt.Color;
+import info.gridworld.actor.Bug;
+
+public class Bishop extends Piece
+{
+	
+	public Bishop(boolean b)
+	{
+		super (b);
+	}
+	
+	public void act()
+	{
+		
+	}
+	
+	
+}
